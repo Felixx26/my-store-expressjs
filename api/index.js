f42433ab94ae956617ce0fd318c257d26d2d04a5
Felix Garcia <fgarcia@tevolv.com>
2023-05-31 19:59:30 -0400

    fix: remove warnings

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const routerApi = require('./routes');
 
-const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler').default;
+const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,4 +18,5 @@ app.use(boomErrorHandler);
 app.use(errorHandler);
 
 
+// eslint-disable-next-line no-console
 app.listen(port, () => console.log(`App listening on port ${port}!`));
--- api/middlewares/error.handler.js
+++ api/middlewares/error.handler.js
@@ -1,14 +1,17 @@
 function logErrors (err, req, res, next) {
+    // eslint-disable-next-line no-console
     console.error(err);
     next(err);
 }
 
-function errorHandler (err, req, res) {
-    res.status(500).json({
-        message: err.message,
-        stack: err.stack
-    });
+// eslint-disable-next-line no-unused-vars
+function errorHandler(err, req, res, next) {
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
 }
+
 function boomErrorHandler (err, req, res, next) {
   if (err.isBoom) {
     const { output } = err;
